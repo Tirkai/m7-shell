024d4af845dda3a7b13a80bfdc2b28e97afdb038
Dmitry <dmitry.tirkai@gmail.com>
2020-07-24 14:01:51 +0300

    Fix: Remove websocket port

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -9,4 +9,4 @@ export const AUTH_TOKEN_HEADER = "X-M7-Authorization-Token";
 export const UPPER_LEVEL_DOMAIN =
     nodeEnv === "development" ? developmentDomain : upperLevelDomain;
 
-export const NOTIFICATIONS_WEBSOCKET_URL = `http://notifications.${UPPER_LEVEL_DOMAIN}:8092/socket.io`;
+export const NOTIFICATIONS_WEBSOCKET_URL = `http://notifications.${UPPER_LEVEL_DOMAIN}/socket.io`;
