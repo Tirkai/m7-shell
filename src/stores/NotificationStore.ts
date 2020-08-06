ec2990099ad2924f3392539adfff199ed85de5e5
Dmitry <burenkov@algont.ru>
2020-08-06 14:16:28 +0300

    Fix: disconnect notification socket

--- src/stores/NotificationStore.ts
+++ src/stores/NotificationStore.ts
@@ -42,53 +42,69 @@ export class NotificationStore {
     }
 
     async fetchNotifications(login: string) {
-        return new Promise(async (resolve) => {
-            const appsResponse = await Axios.post<IJsonRpcResponse<string[]>>(
-                legacyNotificationEndpoint.url,
-                new JsonRpcPayload("getAppIds"),
-            );
+        return new Promise(async (resolve, reject) => {
+            try {
+                const appsResponse = await Axios.post<
+                    IJsonRpcResponse<string[]>
+                >(
+                    legacyNotificationEndpoint.url,
+                    new JsonRpcPayload("getAppIds"),
+                );
 
-            const countResponse = await Axios.post<IJsonRpcResponse<number>>(
-                notificationsEndpoint.url,
-                new JsonRpcPayload("get_count_by_filter", {
-                    filter: {
-                        login: { values: [login] },
-                    },
-                }),
-            );
-
-            this.count = countResponse.data.result;
-
-            const servicedApplicationsIds = appsResponse.data.result;
-
-            const notifications = servicedApplicationsIds.map(async (id) => {
-                const response = await Axios.post<
-                    IJsonRpcResponse<INotificationResponse[]>
+                const countResponse = await Axios.post<
+                    IJsonRpcResponse<number>
                 >(
                     notificationsEndpoint.url,
-                    new JsonRpcPayload("get_list_by_filter", {
+                    new JsonRpcPayload("get_count_by_filter", {
                         filter: {
-                            app_id: {
-                                values: [id],
-                            },
                             login: { values: [login] },
                         },
-                        limit: 5,
-                        offset: 0,
-                        order: [{ field: "ntf_date", direction: "desc" }],
                     }),
                 );
-                return response.data.result.map((item) =>
-                    NotificationFactory.createNotification(item),
+
+                this.count = countResponse.data.result;
+
+                const servicedApplicationsIds = appsResponse.data.result;
+
+                const notifications = servicedApplicationsIds.map(
+                    async (id) => {
+                        const response = await Axios.post<
+                            IJsonRpcResponse<INotificationResponse[]>
+                        >(
+                            notificationsEndpoint.url,
+                            new JsonRpcPayload("get_list_by_filter", {
+                                filter: {
+                                    app_id: {
+                                        values: [id],
+                                    },
+                                    login: { values: [login] },
+                                },
+                                limit: 5,
+                                offset: 0,
+                                order: [
+                                    { field: "ntf_date", direction: "desc" },
+                                ],
+                            }),
+                        );
+                        return response.data.result.map((item) =>
+                            NotificationFactory.createNotification(item),
+                        );
+                    },
                 );
-            });
 
-            Promise.all(notifications).then((values) => {
-                const notificationsList = values.filter((item) => item.length);
-                this.notifications = flatten(notificationsList);
-            });
+                Promise.all(notifications)
+                    .then((values) => {
+                        const notificationsList = values.filter(
+                            (item) => item?.length,
+                        );
+                        this.notifications = flatten(notificationsList ?? []);
+                    })
+                    .catch(() => alert("1"));
 
-            resolve();
+                resolve();
+            } catch (e) {
+                reject();
+            }
         });
     }
 
@@ -97,34 +113,42 @@ export class NotificationStore {
     }
 
     async connectToNotificationsSocket(token: string) {
-        await this.fetchNotifications(this.store.auth.userLogin);
-        if (this.socket === null) {
-            this.socket = io.connect(NOTIFICATIONS_WEBSOCKET_URL, {
-                transportOptions: {
-                    polling: {
-                        extraHeaders: {
-                            [AUTH_TOKEN_HEADER]: token,
+        const reconnect = () => {
+            this.socket?.close();
+            this.socket = null;
+            this.connectToNotificationsSocket(this.store.auth.accessToken);
+        };
+
+        try {
+            await this.fetchNotifications(this.store.auth.userLogin);
+            if (this.socket === null) {
+                this.socket = io.connect(NOTIFICATIONS_WEBSOCKET_URL, {
+                    transportOptions: {
+                        polling: {
+                            extraHeaders: {
+                                [AUTH_TOKEN_HEADER]: token,
+                            },
                         },
                     },
-                },
-            });
+                });
 
-            this.socket.on(
-                "add_notification",
-                (response: INotificationResponse) =>
-                    this.addNotification(response),
-            );
+                this.socket.on(
+                    "add_notification",
+                    (response: INotificationResponse) =>
+                        this.addNotification(response),
+                );
 
-            this.socket.on(
-                "notification_count",
-                (response: INotificationCountResponse) =>
-                    this.updateNotificationCount(response.total),
-            );
+                this.socket.on(
+                    "notification_count",
+                    (response: INotificationCountResponse) =>
+                        this.updateNotificationCount(response.total),
+                );
 
-            this.socket.on("disconnect", () => {
-                this.socket?.close();
-                this.connectToNotificationsSocket(this.store.auth.accessToken);
-            });
+                this.socket.on("disconnect", () => reconnect());
+            }
+        } catch (e) {
+            console.error(e);
+            setTimeout(() => reconnect(), 3000);
         }
     }
 
