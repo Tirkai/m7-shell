30258a42a7668f1aff6e84aea3070946f437a354
Dmitry <burenkov@algont.ru>
2020-11-26 13:37:54 +0300

    Remove localhost apps in dev mode

--- src/registeredApps.tsx
+++ src/registeredApps.tsx
@@ -2,30 +2,11 @@ import { CustomExecutor } from "extensions/CustomExecutor/CustomExecutor";
 import { LaunchQueryBuilder } from "extensions/LaunchQueryBuilder/LaunchQueryBuilder";
 import { strings } from "locale";
 import { Application } from "models/Application";
-import { ExternalApplication } from "models/ExternalApplication";
 import { ShellApplication } from "models/ShellApplication";
 import React from "react";
 import { v4 } from "uuid";
 
 export const registeredApps: Application[] = [
-    new ExternalApplication({
-        id: v4(),
-        name: "Localhost:3000",
-        key: "Localhost:3000",
-        url: `http://localhost:3000`,
-        baseWidth: 800,
-        baseHeight: 650,
-        isVisibleInStartMenu: false,
-    }),
-    new ExternalApplication({
-        id: v4(),
-        name: "Localhost:3001",
-        key: "Localhost:3001",
-        url: `http://localhost:3001`,
-        baseWidth: 800,
-        baseHeight: 650,
-        isVisibleInStartMenu: false,
-    }),
     new ShellApplication({
         id: v4(),
         name: "CustomExecutor",
