5e4033d15e119b4931556caa1310f68a5229efee
Dmitry <burenkov@algont.ru>
2021-07-02 10:24:28 +0300

    Remove deprecated registered application

--- src/registeredApps.tsx
+++ src/registeredApps.tsx
@@ -1,9 +1,7 @@
-import { rocket, terminal } from "assets/icons";
+import { terminal } from "assets/icons";
 import { CustomExecutor } from "extensions/CustomExecutor/CustomExecutor";
-import { LaunchQueryBuilder } from "extensions/LaunchQueryBuilder/LaunchQueryBuilder";
 import { strings } from "locale";
 import { Application } from "models/Application";
-import { ExternalApplication } from "models/ExternalApplication";
 import { ShellApplication } from "models/ShellApplication";
 import React from "react";
 import { v4 } from "uuid";
@@ -18,33 +16,5 @@ export const registeredApps: Application[] = [
         baseHeight: 200,
         isVisibleInStartMenu: false,
         icon: terminal,
-    }),
-    new ShellApplication({
-        id: v4(),
-        name: strings.shellApps.launchQueryBuilder.title,
-        key: "LaunchQueryBuilder",
-        Component: <LaunchQueryBuilder />,
-        baseWidth: 600,
-        baseHeight: 700,
-        isVisibleInStartMenu: false,
-        icon: rocket,
-    }),
-    new ExternalApplication({
-        id: v4(),
-        name: "core.app",
-        key: "Core",
-        url: "/",
-        baseWidth: 600,
-        baseHeight: 700,
-        isVisibleInStartMenu: false,
-    }),
-    new ExternalApplication({
-        id: v4(),
-        name: "core.autodoc",
-        key: "Autodoc",
-        url: window.location.protocol + "//" + window.location.host + "/docs",
-        baseWidth: 600,
-        baseHeight: 700,
-        isVisibleInStartMenu: false,
     }),
 ];
